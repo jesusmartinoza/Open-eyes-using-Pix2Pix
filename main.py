a5da84dea9fc66ed14a9ce25364b66d58cd37ca8
Jesús Martínez <hongo-loco@hotmail.com>
2019-09-01 13:06:39 -0500

    Standarize files names

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from FacePatcher import FacePatcher
 import os
 import random
 
-closed_eyes_files = os.listdir("closed_eyes_dataset/")
+closed_eyes_files = sorted(os.listdir("closed_eyes_dataset/"))
 faces_files = os.listdir("regular_faces/")
+
+# Standarize names
+# for idx, f in enumerate(faces_files):
+#      if 'jpg' in f or 'png' in f:
+#          os.rename("regular_faces/"+f, "regular_faces/face_" + str(idx) + ".jpg")
 
 for idx, f in enumerate(closed_eyes_files):
     if 'jpg' in f or 'png' in f:
